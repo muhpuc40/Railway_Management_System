470072285ebe5b97445a8be2f5e21f068c9d5d67
muhpuc40 <mdhassan49.muh@gmail.com>
2024-06-28 22:44:00 +0600

    git-ignore

--- PROJECT/resources/views/layouts/app.blade.php
+++ PROJECT/resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
                 <img src="{{ asset('images/logo.png') }}" class="img-fluid custom-img-size" alt="Promotional Image">
-                    {{ __('Bangladesh Railway') }}
+                   <b> {{ __('Bangladesh Railway') }}</b>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
