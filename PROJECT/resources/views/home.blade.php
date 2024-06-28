bd27d14c44db2b45cc3e1d368a62fb71ee2b9100
SayeadaSafia2186 <sayeadasafia@gmail.com>
2024-06-28 21:59:17 +0600

    add input section and images in dashboard

--- PROJECT/resources/views/home.blade.php
+++ PROJECT/resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
@@ -13,7 +13,62 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    {{ __('You are logged ') }}
+                    
+                    <!-- Form and Image Section -->
+                    <div class="row">
+                        <!-- Left Column for Form -->
+                        <div class="col-md-8">
+                            <form method="POST" action="{{ url('/search-trains') }}">
+                                @csrf
+                                <div class="row">
+                                    <!-- Left Column for From and To fields -->
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="fromStation">{{ __('From') }}</label>
+                                            <input type="text" class="form-control" id="fromStation" name="fromStation" placeholder="From Station" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="dateOfJourney">{{ __('Date of Journey') }}</label>
+                                            <input type="date" class="form-control" id="dateOfJourney" name="dateOfJourney" required>
+                                        </div>
+                                        
+                                    </div>
+
+                                    <!-- Right Column for Date and Class fields -->
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="toStation">{{ __('To') }}</label>
+                                            <input type="text" class="form-control" id="toStation" name="toStation" placeholder="To Station" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="class">{{ __('Choose Class') }}</label>
+                                            <select class="form-control" id="class" name="class" required>
+                                                <option value="">{{ __('Choose a Class') }}</option>
+                                                <option value="1">{{ __('First Class') }}</option>
+                                                <option value="2">{{ __('Second Class') }}</option>
+                                                <option value="3">{{ __('Sleeper Class') }}</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row justify-content-center mt-3">
+                                    <div class="col-md-4">
+                                        <button type="submit" class="btn btn-primary btn-block">{{ __('Search Trains') }}</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <!-- End of Form Column -->
+
+                        <!-- Right Column for Image -->
+                        <div class="col-md-4">
+                            <img src="{{ asset('images/01.jpg') }}" class="img-fluid" alt="Promotional Image">
+                        </div>
+
+                        <!-- End of Image Column -->
+                    </div>
+                    <!-- End of Form and Image Section -->
+
                 </div>
             </div>
         </div>
