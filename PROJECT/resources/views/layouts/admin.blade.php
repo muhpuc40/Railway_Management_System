0fb39fbcbe4514a349aab97ad9eea068b6599770
muhpuc40 <mdhassan49.muh@gmail.com>
2024-08-08 17:59:07 +0600

    dashboard ->admin

--- PROJECT/resources/views/layouts/admin.blade.php
+++ PROJECT/resources/views/layouts/admin.blade.php
@@ -39,7 +39,7 @@
                 <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
                     <div class="sb-sidenav-menu">
                         <div class="nav">
-                            <a class="nav-link" href="#">
+                            <a class="nav-link" href="{{ url('/Admin') }}">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Dashboard
                             </a>
